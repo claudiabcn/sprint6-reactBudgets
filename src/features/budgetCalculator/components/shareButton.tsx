f28722c4b993e18b2a0382fc0525d5a02e0f6b25
Clàudia López Torres <claudia_4bcn@hotmail.com>
2025-12-24 16:50:55 +0100

    Shared button fail Vercel app

--- src/features/budgetCalculator/components/shareButton.tsx
+++ src/features/budgetCalculator/components/shareButton.tsx
@@ -2,7 +2,7 @@ import { useState } from "react";
 import { ShareButtonProps } from "../../../config/types";
 import Button from "../../../common/components/button";
 
-const ShareButton = ({ getShareableUrl }: ShareButtonProps) => {
+export const ShareButton = ({ getShareableUrl }: ShareButtonProps) => {
   const [copied, setCopied] = useState(false);
 
   const handleShare = async () => {
